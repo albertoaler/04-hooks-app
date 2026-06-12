241b01cd7489d9bf066684feedff03dc78fe0eb8
Alberto Aleman <albertoalemanr@hotmail.com>
2026-06-11 23:01:57 -0600

    feat: add InstagromApp component with optimistic UI and integrate sonner for notifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { Toaster } from 'sonner';
+
 // import { HooksApp } from './HooksApp';
 // import { TrafficLight } from './01-useState/TrafficLight';
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
@@ -10,12 +12,14 @@ import { createRoot } from 'react-dom/client';
 // import { TaskApp } from './05-useReducer/TaskApp';
 // import { ScrambleWords } from './05-useReducer/ScrambleWords';
 // import { MemoHook } from './06-memos/MemoHook';
-import { MemoCounter } from './06-memos/MemoCounter';
+// import { MemoCounter } from './06-memos/MemoCounter';
+import { InstagromApp } from './07-useOptimistic/InstagromApp';
 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster richColors />
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
@@ -25,6 +29,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TaskApp /> */}
     {/* <ScrambleWords /> */}
     {/* <MemoHook /> */}
-    <MemoCounter />
+    {/* <MemoCounter /> */}
+    <InstagromApp />
   </StrictMode>,
 );
